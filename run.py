9bfce8ddd346940bf30e32bdd7966828d9ff6574
danielferrerallc <danielferrerallc@gmail.com>
2025-04-24 12:50:34 +0200

    Add files via upload

---
+++ run.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""
+Punto de entrada de Deep-Live-Cam. Delegará el parseo de argumentos y la ejecución en modules.core.run().
+"""
+from modules import core
+
+if __name__ == "__main__":
+    core.run()
